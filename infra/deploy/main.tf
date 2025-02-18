f2c442d9f667e40aa5e706863270d605191040d0
Stephen-Hallett <stevohallett@gmail.com>
2025-02-18 21:11:43 +1300

    fix: Fix small error in terraform

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_service_plan" "asp" {
 
 resource "azurerm_linux_function_app" "fa" {
   name                       = "fa-${var.project_id}-${var.env}-eau-001"
-  location                   = azurerm_service_plan.asp.location
+  location                   = azurerm_service_plan.asp.rg.location
   resource_group_name        = data.azurerm_resource_group.rg.name
   service_plan_id            = azurerm_service_plan.asp.id
   storage_account_name       = azurerm_storage_account.sa.name
